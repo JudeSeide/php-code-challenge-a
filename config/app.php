d27562a009225a8c98b8f4a6725617af38384af3
Jude Seide <jude.seide@gmail.com>
2017-06-22 00:05:28 -0400

    url fix

--- config/app.php
+++ config/app.php
@@ -7,16 +7,16 @@ return [
 
         'default' => 'freegeoip',
 
-        'ip-api' => env('IP_API', 'ip-api.com/json'),
+        'ip-api' => env('IP_API', 'http://ip-api.com/json'),
 
-        'freegeoip' => env('FREE_GEO_IP', 'freegeoip.net/json')
+        'freegeoip' => env('FREE_GEO_IP', 'http://freegeoip.net/json')
 
     ],
 
 
     'weather' => [
 
-        'url' => env('WEATHER_URL', 'api.openweathermap.org/data/2.5/weather'),
+        'url' => env('WEATHER_URL', 'http://api.openweathermap.org/data/2.5/weather'),
 
         'api_key' => env('WEATHER_API_KEY', '6103b0f582e78c7382bc6b0cdc06deb8')
 
